1493d5911c8f27cdb0b1e1f9df73282c0c9cf153
filippofiocchi <52209617+filippofiocchi@users.noreply.github.com>
2019-07-16 17:16:55 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ PATH=/usr/local/bin:$PATH
 export TOKEN_GITHUB='your github token'
 export TOKEN_BITLY='your bitlytoken'
 export DATABASE_NAME='the name of your database'
+export NAME_ORGANIZZATION='the name of the organizzation on github'
 echo $(date) > tmp.txt
 /usr/local/bin/pipenv run python printer.py > error.txt 2>error2.txt
