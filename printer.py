dbfa6642b2e08fcdc828e00f3a2369a3a0e1d548
filippofiocchi <52209617+filippofiocchi@users.noreply.github.com>
2019-08-07 17:33:22 +0200

    Update printer.py

--- printer.py
+++ printer.py
@@ -27,14 +27,9 @@ class Printer() :
          self._last = url.rfind('/')
          self._n_issue = url[self._last+1:len(url)]
          qrcode_image = qrcode.make(self._urlshort[0])
-         mode = 'RGB' 
-         size = (200, 200)
-         color = (255, 255, 255)
-         new_image = Image.new(mode, size, color)
-         resize_qrcode_image = qrcode_image.resize((75,75), Image.ANTIALIAS)
          new_image.paste(resize_qrcode_image, (int(200/2-100/2), int(200/2-100/2)))
          self._qrcode_logo = '/home/pi/qrcode.png'
-         new_image.save(self._qrcode_logo)
+         qrcode_image.save(self._qrcode_logo)
          self._title = title
          self._repository = reponame
     def print_receipt(self) :
@@ -45,10 +40,10 @@ class Printer() :
              self._printer.size = adafruit_thermal_printer.SIZE_MEDIUM
              self._printer.justify = adafruit_thermal_printer.JUSTIFY_LEFT
              self._printer.feed(2)
-             if len(self._title) < 55:
+             if len(self._title) < 75:
                  self._printer.print(self._title)
              else :
-                 self._printer.print(self._title[0:54]+'...')
+                 self._printer.print(self._title[0:74]+'...')
          else :
              self._printer.size = adafruit_thermal_printer.SIZE_LARGE
              self._printer.justify = adafruit_thermal_printer.JUSTIFY_CENTER
@@ -56,10 +51,10 @@ class Printer() :
              self._printer.size = adafruit_thermal_printer.SIZE_MEDIUM
              self._printer.justify = adafruit_thermal_printer.JUSTIFY_LEFT
              self._printer.feed(2)
-             if len(self._title) < 55:
+             if len(self._title) < 75:
                  self._printer.print(self._title)
              else :
-                 self._printer.print(self._title[0:54]+'...')
+                 self._printer.print(self._title[0:74]+'...')
              time.sleep(7)
              self._printer.feed(1)
              self._printer.size = adafruit_thermal_printer.SIZE_MEDIUM
